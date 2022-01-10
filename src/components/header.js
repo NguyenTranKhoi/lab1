e848897759776bf0ffc52ab415325af684ba6092
khoi <khoidolce@gmail.com>
2022-01-10 18:53:30 +0700

    update

--- src/components/header.js
+++ src/components/header.js
@@ -5,16 +5,16 @@ const header = {
             <img src="../../img/fpt1.png" alt="" class="mx-auto">
         </div>
         <div class="header-bottom bg-orange-600 px-3 flex justify-between">
-        <nav class="">
-          <ul class="flex">
-            <li class="p-3 text-base text-white"><a href="/" class="hover:underline">Trang chủ</a></li>
-            <li class="p-3 text-base text-white"><a href="/about" class="hover:underline">Tuyển sinh</a></li>
-            <li class="p-3 text-base text-white"><a href="/product" class="hover:underline">Chương trình đào tạo</a>
-            </li>
-            <li class="p-3 text-base text-white"><a href="" class="hover:underline">Góc sinh viên</a></li>
-            <li class="p-3 text-base text-white"><a href="" class="hover:underline">Tuyển dụng</a></li>
-          </ul>
-        </nav>
+            <nav class="">
+            <ul class="flex">
+                <li class="p-3 text-base text-white"><a href="/" class="hover:underline">Trang chủ</a></li>
+                <li class="p-3 text-base text-white"><a href="/about" class="hover:underline">Tuyển sinh</a></li>
+                <li class="p-3 text-base text-white"><a href="/product" class="hover:underline">Chương trình đào tạo</a>
+                </li>
+                <li class="p-3 text-base text-white"><a href="" class="hover:underline">Góc sinh viên</a></li>
+                <li class="p-3 text-base text-white"><a href="" class="hover:underline">Tuyển dụng</a></li>
+            </ul>
+            </nav>
         <div class="header-bottom__search p-3">
           <form action="">
             <input type="text" class="w-48" required>
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,14 +1,10 @@
 import banner from "../components/banner";
 import footer from "../components/footer";
-import header from "../components/header";
 import newList from "../components/news";
 
 const HomePage = {
     print() {
         return /* html */ `
-        <div class="header">
-        ${header.print()}
-        </div>
         <div class="banner">
         ${banner.print()}
         </div>
