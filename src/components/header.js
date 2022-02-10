67aaab36a260679b402709234d2b463974e0dac9
khoi <khoidolce@gmail.com>
2022-02-10 12:41:15 +0700

    signin

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const header = {
         <img src="${post.img}" alt="" class="mx-auto">
         `)}
         </div>
-        <div class="header-bottom bg-orange-600 px-3 flex justify-between">
+        <div class="header-bottom bg-orange-600 px-3 flex justify-between items-center">
             <nav class="">
             <ul class="flex">
                 <li class="p-3 text-base text-white"><a href="/" class="hover:underline">Trang chủ</a></li>
@@ -20,6 +20,9 @@ const header = {
                 <li class="p-3 text-base text-white"><a href="/signup" class="hover:underline">Đăng ký</a></li>
             </ul>
             </nav>
+            <ul class="flex">
+                <li><a  id="account-email" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white"></a></li>
+            </ul>
         <div class="header-bottom__search p-3">
           <form action="">
             <input type="text" class="w-48" required>
@@ -28,6 +31,10 @@ const header = {
         </div>
         </div>
         `;
+    },
+    afterRender() {
+        const user = JSON.parse(localStorage.getItem("user"));
+        document.querySelector("#account-email").innerHTML = user.email;
     },
 };
 
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -15,12 +15,21 @@ const Signin = {
     },
     afterRender() {
         const formSignin = document.querySelector("#formSignin");
-        formSignin.addEventListener("submit", (e) => {
+        formSignin.addEventListener("submit", async (e) => {
             e.preventDefault();
-            signin({
+            // call api login
+            const { data } = await signin({
                 email: document.querySelector("#email").value,
                 password: document.querySelector("#password").value,
             });
+            // lưu dữ liệu vào localStorage
+            localStorage.setItem("user", JSON.stringify(data.user));
+            // kiểm tra quyền dựa trên id
+            if (data.user.id === 1) {
+                document.location.href = "/#/admin/news";
+            } else {
+                document.location.href = "/#/";
+            }
         });
     },
 };
